e7789f3e30c7710ad89ea84bfcd1fb8ba54a674d
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-03-30 19:40:01 -0400

    Scene boundry and padding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,17 @@ public class Player : MonoBehaviour
 {
 
     Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
     [SerializeField] float moveSpeed = 5f;
 
+    void Start() {
+        InitBounds();
+    }
     void Update()
     {
         MovePlayer();
@@ -16,12 +25,21 @@ public class Player : MonoBehaviour
 
     void MovePlayer()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value){
        rawInput = value.Get<Vector2>();
        Debug.Log(rawInput);
+    }
+
+    void InitBounds(){
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));  //bottom left corner
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));  //top right corner
     }
 }
