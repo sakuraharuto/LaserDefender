e59a733be04722dbf69920ab6f750591946cb1ce
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-04-02 17:14:30 -0400

    while looping

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,27 +7,32 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] List<WaveConfigSO> waveConfigs;
     WaveConfigSO currentWave;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping;
 
     void Start()
     {
         StartCoroutine(SpawnEnemiesWaves());
     }
 
-    IEnumerator SpawnEnemiesWaves(){
-        foreach (WaveConfigSO wave in waveConfigs)
+    IEnumerator SpawnEnemiesWaves()
+    {
+        do
         {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWayPoint().position, Quaternion.identity, transform);
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWayPoint().position, Quaternion.identity, transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
-        }
-        
+        } while (isLooping);
     }
 
-    public WaveConfigSO GetCurrentWave(){
+    public WaveConfigSO GetCurrentWave()
+    {
         return currentWave;
     }
 }
